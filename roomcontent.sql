6120cb1f1463760bd8e2fd8708723c481b5b27e9
JamesDun2866 <44902009+JamesDun2866@users.noreply.github.com>
2019-04-07 12:50:13 +0100

    Changed IsBooked to No

--- roomcontent.sql
+++ roomcontent.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Apr 07, 2019 at 11:34 AM
+-- Generation Time: Apr 07, 2019 at 11:49 AM
 -- Server version: 5.7.24
 -- PHP Version: 7.2.14
 
@@ -51,9 +51,8 @@ CREATE TABLE IF NOT EXISTS `roomcontent` (
 --
 
 INSERT INTO `roomcontent` (`Room`, `IsBooked`, `IsDisabledAccess`, `RoomID`, `IsProjector`, `LatestBooking`, `BookedDuration`, `Time`, `RoomCapacity`, `RoomBuilding`, `IsComputerLab`, `IsChemicalLab`) VALUES
-('ESK-ALLAM-LT', 'Yes', 'Yes', 1, 'Yes', NULL, NULL, '09:00:00', 500, 'ESK', 'No', 'No'),
-('COHEN-LT3', 'Yes', 'No', 2, 'Yes', NULL, NULL, '10:00:00', 112, 'Cohen', 'No', 'No'),
-('FENNER-052B', 'Yes', 'Yes', 3, 'No', NULL, NULL, NULL, 52, 'Fenner', 'Yes', 'No'),
+('ESK-ALLAM-LT', 'No', 'Yes', 1, 'Yes', NULL, NULL, '09:00:00', 500, 'ESK', 'No', 'No'),
+('COHEN-LT3', 'No', 'No', 2, 'Yes', NULL, NULL, '10:00:00', 112, 'Cohen', 'No', 'No'),
 ('ESK-ALLAM-LT', 'No', 'Yes', 4, 'Yes', NULL, NULL, '10:00:00', 500, 'ESK', 'No', 'No'),
 ('ESK-ALLAM-LT', 'No', 'Yes', 5, 'Yes', NULL, NULL, '11:00:00', 500, 'ESK', 'No', 'No'),
 ('ESK-ALLAM-LT', 'No', 'Yes', 6, 'Yes', NULL, NULL, '12:00:00', 500, 'ESK', 'No', 'No'),
@@ -62,38 +61,38 @@ INSERT INTO `roomcontent` (`Room`, `IsBooked`, `IsDisabledAccess`, `RoomID`, `Is
 ('ESK-ALLAM-LT', 'No', 'Yes', 9, 'Yes', NULL, NULL, '15:00:00', 500, 'ESK', 'No', 'No'),
 ('ESK-ALLAM-LT', 'No', 'Yes', 10, 'Yes', NULL, NULL, '16:00:00', 500, 'ESK', 'No', 'No'),
 ('ESK-ALLAM-LT', 'No', 'Yes', 11, 'Yes', NULL, NULL, '17:00:00', 500, 'ESK', 'No', 'No'),
-('COHEN-LT3', 'Yes', 'No', 12, 'Yes', NULL, NULL, '09:00:00', 112, 'Cohen', 'No', 'No'),
-('COHEN-LT3', 'Yes', 'No', 13, 'Yes', NULL, NULL, '11:00:00', 112, 'Cohen', 'No', 'No'),
-('COHEN-LT3', 'Yes', 'No', 14, 'Yes', NULL, NULL, '12:00:00', 112, 'Cohen', 'No', 'No'),
-('COHEN-LT3', 'Yes', 'No', 15, 'Yes', NULL, NULL, '13:00:00', 112, 'Cohen', 'No', 'No'),
-('COHEN-LT3', 'Yes', 'No', 16, 'Yes', NULL, NULL, '14:00:00', 112, 'Cohen', 'No', 'No'),
-('COHEN-LT3', 'Yes', 'No', 17, 'Yes', NULL, NULL, '15:00:00', 112, 'Cohen', 'No', 'No'),
-('COHEN-LT3', 'Yes', 'No', 18, 'Yes', NULL, NULL, '16:00:00', 112, 'Cohen', 'No', 'No'),
-('COHEN-LT3', 'Yes', 'No', 19, 'Yes', NULL, NULL, '17:00:00', 112, 'Cohen', 'No', 'No'),
-('FENNER-052B', 'Yes', 'Yes', 20, 'No', NULL, NULL, '00:00:00', 52, 'Fenner', 'Yes', 'No'),
-('FENNER-052B', 'Yes', 'Yes', 21, 'No', NULL, NULL, '01:00:00', 52, 'Fenner', 'Yes', 'No'),
-('FENNER-052B', 'Yes', 'Yes', 22, 'No', NULL, NULL, '02:00:00', 52, 'Fenner', 'Yes', 'No'),
-('FENNER-052B', 'Yes', 'Yes', 23, 'No', NULL, NULL, '03:00:00', 52, 'Fenner', 'Yes', 'No'),
-('FENNER-052B', 'Yes', 'Yes', 24, 'No', NULL, NULL, '04:00:00', 52, 'Fenner', 'Yes', 'No'),
-('FENNER-052B', 'Yes', 'Yes', 25, 'No', NULL, NULL, '05:00:00', 52, 'Fenner', 'Yes', 'No'),
-('FENNER-052B', 'Yes', 'Yes', 26, 'No', NULL, NULL, '06:00:00', 52, 'Fenner', 'Yes', 'No'),
-('FENNER-052B', 'Yes', 'Yes', 28, 'No', NULL, NULL, '07:00:00', 52, 'Fenner', 'Yes', 'No'),
-('FENNER-052B', 'Yes', 'Yes', 29, 'No', NULL, NULL, '08:00:00', 52, 'Fenner', 'Yes', 'No'),
-('FENNER-052B', 'Yes', 'Yes', 30, 'No', NULL, NULL, '09:00:00', 52, 'Fenner', 'Yes', 'No'),
-('FENNER-052B', 'Yes', 'Yes', 31, 'No', NULL, NULL, '10:00:00', 52, 'Fenner', 'Yes', 'No'),
-('FENNER-052B', 'Yes', 'Yes', 32, 'No', NULL, NULL, '11:00:00', 52, 'Fenner', 'Yes', 'No'),
-('FENNER-052B', 'Yes', 'Yes', 33, 'No', NULL, NULL, '12:00:00', 52, 'Fenner', 'Yes', 'No'),
-('FENNER-052B', 'Yes', 'Yes', 34, 'No', NULL, NULL, '13:00:00', 52, 'Fenner', 'Yes', 'No'),
-('FENNER-052B', 'Yes', 'Yes', 35, 'No', NULL, NULL, '14:00:00', 52, 'Fenner', 'Yes', 'No'),
-('FENNER-052B', 'Yes', 'Yes', 36, 'No', NULL, NULL, '15:00:00', 52, 'Fenner', 'Yes', 'No'),
-('FENNER-052B', 'Yes', 'Yes', 37, 'No', NULL, NULL, '16:00:00', 52, 'Fenner', 'Yes', 'No'),
-('FENNER-052B', 'Yes', 'Yes', 38, 'No', NULL, NULL, '17:00:00', 52, 'Fenner', 'Yes', 'No'),
-('FENNER-052B', 'Yes', 'Yes', 39, 'No', NULL, NULL, '18:00:00', 52, 'Fenner', 'Yes', 'No'),
-('FENNER-052B', 'Yes', 'Yes', 40, 'No', NULL, NULL, '19:00:00', 52, 'Fenner', 'Yes', 'No'),
-('FENNER-052B', 'Yes', 'Yes', 41, 'No', NULL, NULL, '20:00:00', 52, 'Fenner', 'Yes', 'No'),
-('FENNER-052B', 'Yes', 'Yes', 42, 'No', NULL, NULL, '21:00:00', 52, 'Fenner', 'Yes', 'No'),
-('FENNER-052B', 'Yes', 'Yes', 43, 'No', NULL, NULL, '22:00:00', 52, 'Fenner', 'Yes', 'No'),
-('FENNER-052B', 'Yes', 'Yes', 44, 'No', NULL, NULL, '23:00:00', 52, 'Fenner', 'Yes', 'No');
+('COHEN-LT3', 'No', 'No', 12, 'Yes', NULL, NULL, '09:00:00', 112, 'Cohen', 'No', 'No'),
+('COHEN-LT3', 'No', 'No', 13, 'Yes', NULL, NULL, '11:00:00', 112, 'Cohen', 'No', 'No'),
+('COHEN-LT3', 'No', 'No', 14, 'Yes', NULL, NULL, '12:00:00', 112, 'Cohen', 'No', 'No'),
+('COHEN-LT3', 'No', 'No', 15, 'Yes', NULL, NULL, '13:00:00', 112, 'Cohen', 'No', 'No'),
+('COHEN-LT3', 'No', 'No', 16, 'Yes', NULL, NULL, '14:00:00', 112, 'Cohen', 'No', 'No'),
+('COHEN-LT3', 'No', 'No', 17, 'Yes', NULL, NULL, '15:00:00', 112, 'Cohen', 'No', 'No'),
+('COHEN-LT3', 'No', 'No', 18, 'Yes', NULL, NULL, '16:00:00', 112, 'Cohen', 'No', 'No'),
+('COHEN-LT3', 'No', 'No', 19, 'Yes', NULL, NULL, '17:00:00', 112, 'Cohen', 'No', 'No'),
+('FENNER-052B', 'No', 'Yes', 20, 'No', NULL, NULL, '00:00:00', 52, 'Fenner', 'Yes', 'No'),
+('FENNER-052B', 'No', 'Yes', 21, 'No', NULL, NULL, '01:00:00', 52, 'Fenner', 'Yes', 'No'),
+('FENNER-052B', 'No', 'Yes', 22, 'No', NULL, NULL, '02:00:00', 52, 'Fenner', 'Yes', 'No'),
+('FENNER-052B', 'No', 'Yes', 23, 'No', NULL, NULL, '03:00:00', 52, 'Fenner', 'Yes', 'No'),
+('FENNER-052B', 'No', 'Yes', 24, 'No', NULL, NULL, '04:00:00', 52, 'Fenner', 'Yes', 'No'),
+('FENNER-052B', 'No', 'Yes', 25, 'No', NULL, NULL, '05:00:00', 52, 'Fenner', 'Yes', 'No'),
+('FENNER-052B', 'No', 'Yes', 26, 'No', NULL, NULL, '06:00:00', 52, 'Fenner', 'Yes', 'No'),
+('FENNER-052B', 'No', 'Yes', 28, 'No', NULL, NULL, '07:00:00', 52, 'Fenner', 'Yes', 'No'),
+('FENNER-052B', 'No', 'Yes', 29, 'No', NULL, NULL, '08:00:00', 52, 'Fenner', 'Yes', 'No'),
+('FENNER-052B', 'No', 'Yes', 30, 'No', NULL, NULL, '09:00:00', 52, 'Fenner', 'Yes', 'No'),
+('FENNER-052B', 'No', 'Yes', 31, 'No', NULL, NULL, '10:00:00', 52, 'Fenner', 'Yes', 'No'),
+('FENNER-052B', 'No', 'Yes', 32, 'No', NULL, NULL, '11:00:00', 52, 'Fenner', 'Yes', 'No'),
+('FENNER-052B', 'No', 'Yes', 33, 'No', NULL, NULL, '12:00:00', 52, 'Fenner', 'Yes', 'No'),
+('FENNER-052B', 'No', 'Yes', 34, 'No', NULL, NULL, '13:00:00', 52, 'Fenner', 'Yes', 'No'),
+('FENNER-052B', 'No', 'Yes', 35, 'No', NULL, NULL, '00:00:00', 52, 'Fenner', 'Yes', 'No'),
+('FENNER-052B', 'No', 'Yes', 36, 'No', NULL, NULL, '15:00:00', 52, 'Fenner', 'Yes', 'No'),
+('FENNER-052B', 'No', 'Yes', 37, 'No', NULL, NULL, '16:00:00', 52, 'Fenner', 'Yes', 'No'),
+('FENNER-052B', 'No', 'Yes', 38, 'No', NULL, NULL, '17:00:00', 52, 'Fenner', 'Yes', 'No'),
+('FENNER-052B', 'No', 'Yes', 39, 'No', NULL, NULL, '18:00:00', 52, 'Fenner', 'Yes', 'No'),
+('FENNER-052B', 'No', 'Yes', 40, 'No', NULL, NULL, '19:00:00', 52, 'Fenner', 'Yes', 'No'),
+('FENNER-052B', 'No', 'Yes', 41, 'No', NULL, NULL, '20:00:00', 52, 'Fenner', 'Yes', 'No'),
+('FENNER-052B', 'No', 'Yes', 42, 'No', NULL, NULL, '21:00:00', 52, 'Fenner', 'Yes', 'No'),
+('FENNER-052B', 'No', 'Yes', 43, 'No', NULL, NULL, '22:00:00', 52, 'Fenner', 'Yes', 'No'),
+('FENNER-052B', 'No', 'Yes', 44, 'No', NULL, NULL, '23:00:00', 52, 'Fenner', 'Yes', 'No');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
