6d7c7cf088e0d7ede6e68e503ccc542ecefcd78c
Antony Leons <antony@leons.dev>
2019-04-09 17:57:06 +0100

    Update loop.php

--- SQL/loop.php
+++ SQL/loop.php
@@ -7,7 +7,7 @@ $dbhost = "localhost";
          $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $db);
 
          $i = 1;
-         while($i <= 365){
+         while($i <= 100){
 
              $sql = "INSERT INTO roomcontent ( `Room` ,`IsBooked`, `IsDisabledAccess`, `IsProjector`, `Date`, `Time`, `RoomCapacity`, `RoomBuilding`, `IsComputerLab`, `IsChemicalLab` ) SELECT `Room` ,`IsBooked`, `IsDisabledAccess`, `IsProjector`,(SELECT DATE_ADD('2019-04-09', INTERVAL '$i' DAY)), `Time`, `RoomCapacity`, `RoomBuilding`, `IsComputerLab`, `IsChemicalLab` FROM roomcontent";
             mysqli_query($conn, $sql);
